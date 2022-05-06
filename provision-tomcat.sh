6c338dd1f113f3e1bd827b4b297e951cad7ca0ee
Toscano <paolo.toscano@capgemini.com>
2022-05-06 17:33:11 +0100

    Added new tomcat8

--- provision-tomcat.sh
+++ provision-tomcat.sh
@@ -1,5 +1,15 @@
-#!/mingw64/bin/git
+#!/bin/bash
 
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y nano git openjdk-7-jdk openjdk-7-jre-headless
+
+cd /usr/local
+wget https://dlcdn.apache.org/tomcat/tomcat-8/v8.5.78/bin/apache-tomcat-8.5.78.tar.gz
+tar -xzvf apache-tomcat-8.5.78.tar.gz
+ln -s apache-tomcat-8.5.78 tomcat
+
+cp /vagrant/tomcat8 /etc/init.d/tomcat8
+chmod 755 /etc/init.d/tomcat8
+update-rc.d tomcat8 defaults
+service tomcat8 start
